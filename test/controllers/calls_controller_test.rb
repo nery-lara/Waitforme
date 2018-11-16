0a93fc25ff1a9b83583e331f98348a6e62d3d48e
Jacky Zheng <jzheng1126@yahoo.com>
2018-11-15 18:27:28 -0800

    Modified initial test to actually run.

--- test/controllers/calls_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CallsControllerTest < ActionDispatch::IntegrationTest
-  test "should get reply" do
-    get calls_reply_url
-    assert_response :success
-  end
-
-end
