e192721b58c538a6f88dc3e1fe67c789fd747122
sitaochen <sitaochen@umail.ucsb.edu>
2019-02-04 17:58:31 -0800

    "change controller"
    Co-Authored-By: yuhaozhang <yuhaozhang@ucsb.edu>

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -15,16 +15,48 @@ class IntentHandlersController < ApplicationController
       when "UserInput"
         response = input.slots["businessType"].value
         message = "You said, #{response}"
-        #make a call
-        account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-        auth_token = 'your_auth_token'
-        @client = Twilio::REST::Client.new(account_sid, auth_token)
-        call = @client.calls.create(
-          url: 'http://demo.twilio.com/docs/voice.xml',
-          to: twilio_number,
-          from: user_number
-        )
+        start(response)
       end
     end
+  end
+
+  def start(business_number)
+    twilio_number = ''
+    call = @client.calls.create(
+      url:@url + "alexa/call_user",
+      to:business_number,
+      from:twilio_number
+    )
+    # forward_call = ForwardCall.new(business_number)
+    # response = VoiceResponse.new(forward_call)
+    # render xml: response.xml
+  end
+
+  def answered
+    @@business_callSid = params['CallSid']
+    logger.debug 'business callsid ' + @@business_callSid
+    answered_msg = Answered.new
+    response = VoiceResponse.new(answered_msg)
+    render xml: response.xml
+  end
+
+  def call_user
+    call = @client.calls.create(
+      url:@url + "calls/answered",
+      to:phone_number,
+      from:twilio_number # another twilio number?
+    )
+    # forward_call = ForwardCall.new(phone_number)
+    # response = VoiceResponse.new(forward_call)
+    # render xml: response.xml
+  end
+
+
+  private
+  def boot_twilio
+    account_sid = ''
+    auth_token = ''
+    @url = ''
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 end
