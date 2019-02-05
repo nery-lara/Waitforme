2183b7c7aa223c17801a4f6e95e6bc61281d4947
Jacky Zheng <jzheng1126@yahoo.com>
2019-02-04 16:47:25 -0800

    Fixed project id and removed unecessary debug.

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -20,7 +20,6 @@ class CallsController < ApplicationController
   end
 
   def dial
-    logger.debug 'inside dial'
     boot_twilio
     @@dial_number = params['Digits']
     @call = @@client.calls.create(
@@ -150,8 +149,6 @@ class CallsController < ApplicationController
   def boot_twilio
     account_sid = ENV['ACCOUNT_SID']
     auth_token = ENV['AUTH_TOKEN']
-    logger.debug account_sid
-    logger.debug auth_token
     @@client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 end
