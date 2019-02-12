603cbd82e6c8decea918b62dd81c4f63c5b24e1d
Angel Castro <angel90650@gmail.com>
2019-02-11 21:43:25 -0800

    replace global variables with redis cached session model

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,94 +1,109 @@
 class CallsController < ApplicationController
+  include ApplicationHelper
   skip_before_action :verify_authenticity_token
-  @@user_number = ""
-  @@has_called = ""
-  @@user_callSid = ""
-  @@business_callSid = ""
-  @@waitforme = false
-  @@url = 'http://25a3a11c.ngrok.io'
+  @@url = 'http://f784f662.ngrok.io'
   Rails.logger = Logger.new(STDOUT)
 
   def start
+    session = Session.new
+    session.user.name = create_username
+    session.conference.name = 'conference_' + session.user.name
+    logger.debug 'user endpoint is ' + session.user.name
+    session.user.number = params['From']
+    logger.debug 'session number' + session.user.number
     logger.debug 'inside start'
-    @@user_number = params['From']
-    @@user_callSid = params['CallSid']
-    logger.debug 'user callsid ' + @@user_callSid
-    start_conference = StartConference.new
-    response = VoiceResponse.new(start_conference)
+    session.user.sid = params['CallSid']
+    logger.debug 'user callsid ' + session.user.sid
+    start_conference = StartConference.new(session.user.name)
+    response = VoiceResponse.new(start_conference, session)
+    store_session(session.user.name, session)
     render xml: response.xml
   end
 
   def dial
     boot_twilio
-    @@dial_number = params['Digits']
-    @call = @@client.calls.create(
-      url: @@url + "/calls/answered",
-      to: @@dial_number,
-      from: @@user_number)
-    forward_call = ForwardCall.new(@@dial_number)
-    response = VoiceResponse.new(forward_call)
+    logger.debug 'user params' + params['user']
+    session = fetch_session(params[:user])
+    session.business.number = params['Digits']
+    logger.debug 'dial name' + session.user.name
+    call = @@client.calls.create(
+      url: @@url + "/calls/answered" + '/' + session.user.name,
+      to: session.business.number,
+      from: session.user.number)
+    forward_call = ForwardCall.new(session.business.number, session.user.name)
+    response = VoiceResponse.new(forward_call, session)
+    store_session(session.user.name, session)
     render xml: response.xml
+
   end
 
   def answered
-    @@business_callSid = params['CallSid']
-    logger.debug 'business callsid ' + @@business_callSid
+    session = fetch_session(params[:user])
+    session.business.sid = params['CallSid']
+    logger.debug 'business callsid ' + session.business.sid
     answered_msg = Answered.new
-    response = VoiceResponse.new(answered_msg)
+    response = VoiceResponse.new(answered_msg, session)
+    store_session(session.user.name, session)
     render xml: response.xml
   end
 
   def conference
+    session = fetch_session(params[:user])
     @event = params["StatusCallbackEvent"]
-    if @event == "participant-leave" and params['CallSid'] == @@user_callSid
-      logger.debug 'conference sid: ' + @@conference_Sid
+    if @event == "participant-leave" and params['CallSid'] == session.user.sid
+      logger.debug 'conference sid: ' + session.conference.sid
       logger.debug 'user left conference'
     end
 
-    if @event == "participant-leave" and params['CallSid'] == @@business_callSid
+    if @event == "participant-leave" and params['CallSid'] == session.business.sid
       logger.debug 'end the whole thing, the business hung up'
-      hangup_user
+      hangup_user(session)
     end
 
     if @event == "participant-join"
       logger.debug 'someone is joining the conference'
-      if params["CallSid"] == @@user_callSid
+      if params["CallSid"] == session.user.sid
         logger.debug 'user is joining the conference'
         logger.debug 'their callsid is ' + params['CallSid']
-        @@conference_Sid = params['ConferenceSid']
-        logger.debug 'conference Sid is:' + @@conference_Sid
-        user = @@client.conferences(@@conference_Sid).fetch
+        session.conference.sid = params['ConferenceSid']
+        logger.debug 'conference Sid is:' + session.conference.sid
+        user = @@client.conferences(session.conference.sid).fetch
         logger.debug 'here' + user.friendly_name
-        announce = @@client.conferences(@@conference_Sid).participants(@@user_callSid).update(announce_url: @@url + "/calls/connect")
+        announce = @@client.conferences(session.conference.sid).participants(session.user.sid).update(announce_url: @@url + "/calls/connect" + '/' + session.user.name)
       end
-      if params["CallSid"] == @@business_callSid
+      if params["CallSid"] == session.business.sid
         logger.debug 'business is joining the conference'
         logger.debug 'their callsid is ' + params['CallSid']
       end
     end
+    store_session(session.user.name, session)
   end
 
   def wait_for_me
+    session = fetch_session(params[:user])
     #detect when off hold
     #call user back
     call = @@client.calls.create(
-      url: @@url + "/calls/rejoin_conference",
-      from: @@dial_number,
-      to: @@user_number
+      url: @@url + "/calls/rejoin_conference/" + session.user.name,
+      from: session.business.number,
+      to: session.user.number
     )
+    store_session(session.user.name, session)
     #join conference
   end
 
   def connect
+    session = fetch_session(params[:user])
     announce = Announcement.new
-    response = VoiceResponse.new(announce)
+    response = VoiceResponse.new(announce, session)
     render xml: response.xml
   end
 
   def confirm_wait
+    session = fetch_session(params[:user])
     input = params['Digits']
-    confirm_wait = ConfirmWait.new(input)
-    response = VoiceResponse.new(confirm_wait)
+    confirm_wait = ConfirmWait.new(input, session.user.name)
+    response = VoiceResponse.new(confirm_wait, session)
     render xml: response.xml
   end
 
@@ -101,32 +116,35 @@ class CallsController < ApplicationController
   end
 
   def rejoin_conference
-    @@user_callSid = params['CallSid']
+    session = fetch_session(params[:user])
+    session.user.sid = params['CallSid']
     rejoin_conference = RejoinConference.new
-    response = VoiceResponse.new(rejoin_conference)
+    response = VoiceResponse.new(rejoin_conference, session)
+    store_session(session.user.name, session)
     render xml: response.xml
   end
 
   def check_wait_or_exit
+    session = fetch_session(params[:user])
     if params['CallStatus'] == 'completed'
       logger.debug 'user call completed, hang up business'
-        hangup_business
+        hangup_business(session)
     else
       logger.debug 'user call not completed'
       response = Twilio::TwiML::VoiceResponse.new do |response|
-        response.gather(action: '/calls/confirm_wait', method: 'POST', numdigits: 2)
-        response.redirect('/calls/rejoin_conference')
+        response.gather(action: '/calls/confirm_wait'+ '/' + session.user.name, method: 'POST', numdigits: 2)
+        response.redirect('/calls/rejoin_conference' + '/' + session.user.name)
       end
       render xml: response.to_s
     end
   end
 
-  def hangup_business
-    @@client.calls(@@business_callSid).update(status: 'completed')
+  def hangup_business(session)
+    @@client.calls(session.business.sid).update(status: 'completed')
   end
 
-  def hangup_user
-    @@client.calls(@@user_callSid).update(status: 'completed')
+  def hangup_user(session)
+    @@client.calls(session.user.sid).update(status: 'completed')
   end
 
   def status_change
@@ -135,19 +153,16 @@ class CallsController < ApplicationController
     status = params['CallStatus']
     logger.debug 'call status changed'
     logger.debug 'call sid:' + callsid
-    if callsid == @@user_callSid
-      logger.debug 'user status changed'
-      if status == 'completed'
-        logger.debug 'user status is complete'
-      end
+    if status == 'completed'
+      logger.debug 'user status is complete'
     end
   end
 
 
   private
   def boot_twilio
-    account_sid = ''
-    auth_token = ''
+    account_sid = 'AC14a0fc7958eb5a457b937744ac590ac4'
+    auth_token = '1ac75e253415d780d1a29466adfaee02'
     @@client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 end
