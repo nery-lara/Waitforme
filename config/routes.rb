65f05c801ec334951af1903976efc42221b437fa
Angel Castro <angel90650@gmail.com>
2019-02-11 21:41:43 -0800

    modify routes to handle user ids

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   post 'calls/start'
-  post 'calls/dial'
-  post 'calls/answered'
-  post 'calls/conference'
-  post 'calls/wait_for_me'
-  post 'calls/confirm_wait'
-  post 'calls/rejoin_conference'
-  post 'calls/hangup'
-  post 'calls/connect'
+  post 'calls/dial/:user', to: 'calls#dial'
+  post 'calls/answered/:user', to: 'calls#answered'
+  post 'calls/conference/:user', to: 'calls#conference'
+  post 'calls/wait_for_me/:user', to: 'calls#wait_for_me'
+  post 'calls/confirm_wait/:user', to: 'calls#confirm_wait'
+  post 'calls/rejoin_conference/:user', to: 'calls#rejoin_conference'
+  post 'calls/hangup/:user', to: 'calls#hangup'
+  post 'calls/connect/:user', to: 'calls#connect'
   post 'calls/status_change'
-  post 'calls/check_wait_or_exit'
+  post 'calls/check_wait_or_exit/:user', to: 'calls#check_wait_or_exit'
 
 end
