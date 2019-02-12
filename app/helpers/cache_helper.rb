12d4cdbed5768a6383393d70c5c58ef0627ee875
Angel Castro <angel90650@gmail.com>
2019-02-11 21:42:10 -0800

    add cache helper methods

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -8,4 +8,11 @@ module CacheHelper
     $redis.set(key, value)
   end
 
+  def cache_incr(key)
+    $redis.incr(key)
+  end
+
+  def cache_set_add(key, value)
+    $redis.sadd(key, value)
+  end
 end
