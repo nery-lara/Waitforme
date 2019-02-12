3f6b7cdf2c65470876c03a846772df9f1fca465b
Angel Castro <angel90650@gmail.com>
2019-02-11 21:42:43 -0800

    modify app helper to handle updated session model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,29 @@
 module ApplicationHelper
   include CacheHelper
+
+  def create_username
+    cache_incr('user_count')
+    username = 'user' + cache_get('user_count')
+  end
+
+  def add_user_key(user, value)
+    cache_set_add(user, value)
+  end
+  def get_user_key(value)
+    #look for value in cache_set
+    #
+  end
   def fetch_session(key)
     session_json = cache_get(key)
     session_parsed = JSON.parse(session_json)
     session = Session.new
     session.user.number = session_parsed['user']['number']
     session.user.sid = session_parsed['user']['sid']
+    session.user.name = session_parsed['user']['name']
     session.business.number = session_parsed['business']['number']
     session.business.sid = session_parsed['business']['sid']
     session.conference.sid = session_parsed['conference']['sid']
+    session.conference.name = session_parsed['conference']['name']
     session
   end
 
