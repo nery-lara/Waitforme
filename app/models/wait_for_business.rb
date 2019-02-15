749d37845d88715d77b48666fa82685ec0aa7994
Angel Castro <angel90650@gmail.com>
2019-02-14 19:15:32 -0800

    fix wait for business model

--- app/models/wait_for_business.rb
+++ app/models/wait_for_business.rb
@@ -1,11 +1,13 @@
 class WaitForBusiness
 
-  def initialize
-    @business_rejoin_conference = '/calls/business_rejoin_conference'
+  def initialize(user_endpoint)
+    @user_endpoint = user_endpoint
+    @business_rejoin_conference = '/calls/business_rejoin_conference' + '/' + user_endpoint
     @post = 'POST'
     @speech = 'speech'
     @wait_for_business = '/calls/wait_for_business'
     @speech_timeout = 1
+
   end
 
   attr_reader :business_rejoin_conference, :post, :speech, :wait_for_business, :speech_timeout
