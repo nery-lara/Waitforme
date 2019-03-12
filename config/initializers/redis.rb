859e77e016087b2be0670cae8ec099c4852f735e
Angel Castro <angel90650@gmail.com>
2019-03-12 02:35:16 -0700

    update redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,2 @@
-$redis = Redis::Namespace.new("waitforme", :redis =>Redis.new)
+$redis = Redis::Namespace.new("waitforme",
+  :redis =>Redis.new(host: "waitforme-redis-001.dop21n.0001.use2.cache.amazonaws.com", port: 6379))
