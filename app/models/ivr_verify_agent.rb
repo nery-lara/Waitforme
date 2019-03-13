edb860a4bf3f61e6b77f59d92cf2232dfa54c27a
Angel Castro <angel90650@gmail.com>
2019-03-12 17:23:50 -0700

    change ivr number back

--- app/models/ivr_verify_agent.rb
+++ app/models/ivr_verify_agent.rb
@@ -2,8 +2,8 @@ class IvrVerifyAgent
 
   def initialize(number, user)
     @call_from = number
-    #@business_agent = '+18056405991'
-    @business_agent = '+14156106787'
+    @business_agent = '+18056405991'
+    #@business_agent = '+14156106787'
     @ivr_main_menu = '/ivr/main_menu' + '/' + user
   end
 
